e33a149fbea6b39916548ff52449ed45e446743e
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-08-24 01:43:29 +1200

    init

---
+++ src/backend/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const todoRoutes = express.Router();
+const PORT = process.env.PORT || 4000;
+
+require('./database');
+
+let Todo = require('./todo.model');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));           
+app.use(bodyParser.json());
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+    console.log("req.body:" + req.body.todo_description);
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
