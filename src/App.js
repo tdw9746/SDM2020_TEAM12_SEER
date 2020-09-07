92d7190d3ef64cd932c4f92be2b468ce3edd4158
ruaia.kr <ruaia.kr>
2020-09-07 16:58:58 +1200

    test from R

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
                   <Link to="/create" className="nav-link">Create Todo</Link>
                 </li>
                 <li>
-                  Testing area  : seq 6
+                  Testing area  : seq 10
                 </li>
               </ul>
             </div>
