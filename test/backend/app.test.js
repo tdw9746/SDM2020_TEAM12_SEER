cff7903c947a5108131a3c58d9b51a04cff0babe
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-11 12:11:23 +1200

    fixed unit test

--- test/backend/app.test.js
+++ test/backend/app.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('backend tests - should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(1), 1);
+      assert.equal([1, 2, 3].indexOf(1), 0);
     });
   });
 }); 
