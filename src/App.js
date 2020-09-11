7600501c309c8c91fd30db56f2d191e84a5d42a6
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-11 13:01:26 +1200

    test Travis CI builds

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
                   <Link to="/create" className="nav-link">Create Todo</Link>
                 </li>
                 <li>
-                  Testing area  : seq 21
+                  Testing area  : seq 22
                 </li>
               </ul>
             </div>
