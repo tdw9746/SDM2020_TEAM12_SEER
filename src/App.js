b03b750b10830d8770b7ff02a9aa228917d27d55
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-11 12:15:47 +1200

    test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
                   <Link to="/create" className="nav-link">Create Todo</Link>
                 </li>
                 <li>
-                  Testing area  : seq 10
+                  Testing area  : seq 20
                 </li>
               </ul>
             </div>
