a59fca11b7f22b2e2077d7a69dbc578e75637e4d
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-24 23:00:51 +1200

    unit test for search

---
+++ test/backend/route/search.test.js
@@ -0,0 +1,21 @@
+const searchRoute = require("../../../src/backend/routes/search");
+
+var assert = require('assert');
+
+describe('Test Suite 1', function() {
+    it('Test 1', function() {
+        let jsonReturn =   searchRoute.seerSearchJson("title", "author", "method") ;
+        /*
+        {
+            {"title":}
+        }
+        */
+        // See if there is result
+        assert.ok( jsonReturn[0].title == "", "This shouldn't fail");
+    })
+
+    it('Test 2', function() {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    })
+})
