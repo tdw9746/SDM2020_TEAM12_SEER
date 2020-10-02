b41efae379832b2cfbb922dc626d7770ff2e658d
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-10-02 20:27:46 +1300

    UI design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { Header, Icon } from 'semantic-ui-react'
+
 import Search from './components/Search.component';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Search/>
+      <br/>
+      <Header as='h1'>
+        <Icon name='lab' />
+        <Header.Content>Software Engineering Evidence Repository(SEER)</Header.Content>
+      </Header>
+      <Search />
     </div>
   );
 }
