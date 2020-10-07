dbdb5f1c2e017ba29d32d6bb7bb4127da12da987
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-10-08 00:46:40 +1300

    minor UI change

--- src/components/Search.component.js
+++ src/components/Search.component.js
@@ -48,7 +48,7 @@ class Search extends Component {
 
     return(
       <div>
-        <h1>Search articles</h1>
+        <h1 align="left">Search articles</h1>
         <SearchCondition showSeerArticleList={this.showSeerArticleList}/>
         <ListSeerArticle seerArticles={seerArticles}/>
       </div>
